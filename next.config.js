aea7a50a8950dfda67337681ba862e3404a6de63
zenchantlive <jordanlive121@gmail.com>
2024-08-12 00:58:16 -0700

    Update next.config.js with Auth0 cookie and session settings

--- next.config.js
+++ next.config.js
@@ -18,4 +18,25 @@ module.exports = {
       },
     ];
   },
+  serverRuntimeConfig: {
+    auth0: {
+      secret: process.env.AUTH0_SECRET,
+      baseURL: process.env.AUTH0_BASE_URL,
+      issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      session: {
+        cookieSecret: process.env.AUTH0_SECRET,
+        cookieLifetime: 60 * 60 * 8,
+        cookieSameSite: 'lax',
+        storeIdToken: false,
+        storeAccessToken: false,
+        storeRefreshToken: false,
+      },
+      oidcClient: {
+        httpTimeout: 2500,
+        clockTolerance: 10000,
+      },
+    },
+  },
 };
